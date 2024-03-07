490c0323aa0e040eac19a1de57b75306f35862dd
sameet <sameetsabu1@gmail.com>
2024-03-07 13:00:56 +0530

    appbar created

---
+++ components/appbar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+const Appbar = () => {
+  return (
+    <div className="flex justify-between p-5 shadow-md">
+      <div
+        className="mt-2
+      "
+      >
+        <Image src={"/logo.jpg"} alt="Logo" width={60} height={60} />
+      </div>
+      <div className="flex gap-4 text-sm font-semibold">
+        <div className="mt-2">Crypto Taxes</div>
+        <div className="mt-2">Free Tools</div>
+        <div className="mt-2">Resource Center</div>
+        <div>
+          <button className="w-full h-full bg-gradient-to-r from-blue-500 to-blue-700 rounded-lg text-white p-2 hover:bg-blue-800 transition duration-300 ease-out">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Appbar;
